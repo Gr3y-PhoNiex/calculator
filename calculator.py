e6c154b3ebc54a91618c583ca9cf06849dd2ecf2
Gr3y-PhoNiex <65476099+Gr3y-PhoNiex@users.noreply.github.com>
2020-05-19 03:47:47 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,22 @@
-a =int(input("Enter First Number:"))
-b =int(input("Enter Second Number:"))
-z =a+b
-x =a-b
-y =a*b
-v =a/b
-print("{0} + {1} = {2}".format( a , b, z))
-print("{0} - {1} = {2}".format( a , b , x))
-print("{0} * {1} = {2}".format( a , b , y))
-print("{} / {} = {}".format( a , b , v))
+name = raw_input("Please Enter Your Name:")
+print("Welcome To My Calculator",name)
+a = int(input("Enter First Number:"))
+b = int(input("Enter Second Number:"))
+x = a+b
+y = a-b
+z = a*b
+v = a/b
+print("What You Want To Do?")
+print("1.Plus")
+print("2.Minus")
+print("3.Multiply")
+print("4.Division")
+answer = int(input("Please Enter Only Number:"))
+if answer == 1:
+    print("{} + {} = {}".format(a , b , x))
+elif answer == 2:
+    print("{} - {} = {}".format(a , b , y))
+elif answer == 3:
+    print("{} * {} = {}".format(a , b , z))
+elif answer == 4:
+    print("{} / {} = {}".format(a , b , v))
